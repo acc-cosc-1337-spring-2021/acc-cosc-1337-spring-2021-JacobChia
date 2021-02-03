e28e88ef97feeba897d486265ef393a02d6bcd20
JacobChia <jacob.chia@g.austincc.edu>
2021-02-03 16:42:05 -0600

    assignment 1

--- src/classwork/01_assign/main.cpp
+++ src/classwork/01_assign/main.cpp
@@ -2,10 +2,21 @@
 
 //Write include for capturing input from keyboard and displaying output to screen
 
-
+#include <iostream>
+#include "types.h"
+using namespace std;
 
 int main() 
 {
+	int num;
+	cin >> num;
+
+	int result = multiply_numbers(num);
+    cout << result << "\n";
+
+	int num1 = 4;
+	result = multiply_numbers(num1);
+	cout << result << "\n";
 
 	return 0;
 }
