66a26fe958284ce6ed0b4eb193317ed9b347bcc8
JacobChia <jacob.chia@g.austincc.edu>
2021-02-09 21:41:17 -0600

    hw2

--- src/homework/01_variables/main.cpp
+++ src/homework/01_variables/main.cpp
@@ -1,6 +1,7 @@
 ﻿//write include statements
-
-
+#include "variables.h"
+#include <iostream>
+using namespace std;
 //write namespace using statement for cout
 
 
@@ -9,7 +10,19 @@ Call multiply_numbers with 10 and 10 parameter values and display function resul
 */
 int main()
 {
+	double meal_amount, tip_rate, tip_amount, tax_amount, total;
 	
+	cin >> meal_amount;
+	tax_amount = get_sales_tax_amount(meal_amount);
+
+	cin >> tip_rate;
+	tip_amount = get_tip_amount(meal_amount, tip_rate);
+	total = tip_amount + tax_amount + meal_amount;
+
+	cout << "Meal Amount:    " << meal_amount << "\n";
+	cout << "Sales Tax:      " << tax_amount << "\n";
+	cout << "Tip Amount:     " << tip_amount << "\n";
+	cout << "Total:          " << total << "\n";
 
 	return 0;
 }
