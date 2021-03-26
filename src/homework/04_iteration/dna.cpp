adbd509afe8c62fb6890e0dcc5edb87f37e7c3d6
JacobChia <jacob.chia@g.austincc.edu>
2021-03-25 22:32:24 -0500

    hw04

--- src/homework/04_iteration/dna.cpp
+++ src/homework/04_iteration/dna.cpp
@@ -1,4 +1,7 @@
 #include "dna.h"
+#include <string>
+#include <iostream>
+using namespace std;
 /*
 Write code for function get_gc_content that accepts
 a const reference string parameter and returns a double.
@@ -6,7 +9,18 @@ Calculate GC content:
 Iterate string count Gs and Cs, divide count by string length.
 Return quotient.
 */
-
+double get_gc_content(const string& dna)
+{
+    int count = 0;
+    int len = dna.length();
+    for(int i=0;i<len;i++) {
+        if(dna[i] == 'G')
+            count++;
+        else if (dna[i] == 'C')
+            count++;
+    }
+    return count/len;
+}
 
 
 
@@ -14,7 +28,15 @@ Return quotient.
 Write code for function get_reverse_string that
 accepts a string parameter and returns a string reversed.
 */
-
+string get_reverse_string(string dna)
+{
+    string result = "";
+    int len = dna.length();
+    for (int i=0;i<len;i++) {
+        result = dna[i] + result;
+    }
+    return result;
+}
 
 
 /*
@@ -27,4 +49,27 @@ b. iterate local string variable and
 c. return string
 
 */
-
+string get_dna_complement(string dna)
+{
+    string tmp = get_reverse_string(dna);
+    int len = tmp.length();
+    for (int i=0;i<len;i++) {
+        switch(tmp[i]) {
+            case 'T':
+                tmp[i] = 'A';
+                break;
+            case 'A':
+                tmp[i] = 'T';
+                break;
+            case 'C':
+                tmp[i] = 'G';
+                break;
+            case 'G':
+                tmp[i] = 'C';
+                break;
+            default:
+                break;   
+        }
+    }
+    return tmp;
+}
